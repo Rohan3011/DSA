6c879bacb596324e9eab2476a9d9f01000d8f768
rohan kamble <76813454+Rohan3011@users.noreply.github.com>
2021-09-11 20:38:29 +0530

    standard BS problem

---
+++ AGGRCOW_BS.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int binarySearch(int arr[] , int n , int c) {
+	int l = 0 , r = arr[n - 1] - arr[0] , ans = 0 ;
+	while (l <= r) {
+		int mid = l + (r - l) / 2 , placed = 1 , prev = 0  ;
+		for (int i = 0 ; i < n ; i++) {
+			if (arr[prev] + mid <= arr[i]) {
+				prev = i ;
+				placed++ ;
+			}
+		}
+		if (placed >= c) {
+			ans = mid ;
+			l = mid + 1 ;
+		}
+		else
+			r = mid - 1;
+	}
+	return ans ;
+}
+
+int main() {
+	int t ;
+	cin >> t ;
+	while (t--) {
+		int n , c ;
+		cin >> n >> c ;
+		int stalls[n] ;
+		for (auto &i : stalls) cin >> i ;
+		sort(stalls , stalls + n);
+		cout << binarySearch(stalls , n , c) << "\n" ;
+	}
+
+}
