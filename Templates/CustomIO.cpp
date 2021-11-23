486fd7ef581731ed2ddd885dd0ecac16669fa9ca
rohan kamble <76813454+Rohan3011@users.noreply.github.com>
2021-11-23 20:03:12 +0530

    Custom IO

---
+++ Templates/CustomIO.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+// Standard input and output template
+template<typename... T>void read(T&... args) {((cin >> args), ...);}
+template<typename... T>void print(T&&... args) {((cout << args << " "), ...);}
+template<typename... T>void println(T&&... args) {((cout << args << "\n"), ...);}
+template<typename... T>void err(T&&... args) {((cerr << args << " "), ...);}
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
